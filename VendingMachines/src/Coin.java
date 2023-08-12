31af20d515979530faf7a431c0607cb615c5297f
FedasaBote <fedasagete@gmail.com>
2023-08-12 14:23:50 +0300

    Add files via upload

---
+++ VendingMachines/src/Coin.java
@@ -0,0 +1,12 @@
+public enum Coin {
+    PENNY(1),NICKLE(5),DIME(10),QUARTER(25);
+
+    private final int denomination;
+    private Coin(int denomination){
+        this.denomination =denomination;
+    }
+
+    public int getDenomination() {
+        return denomination;
+    }
+}
---
+++ VendingMachines/src/NotSufficientChangeException.java
@@ -0,0 +1,11 @@
+public class NotSufficientChangeException extends RuntimeException {
+    private String message;
+
+    public NotSufficientChangeException(String message){
+        this.message=message;
+    }
+    @Override
+    public String getMessage(){
+        return message;
+    }
+}
